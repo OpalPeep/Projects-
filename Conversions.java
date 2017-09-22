8620d4f241423d6fc67b3a3135ffa3591e520237
OpalPeep <32079154+OpalPeep@users.noreply.github.com>
2017-09-21 22:28:23 -0700

    Vs 1.1 to 1.2
    Bug Tested, and got rid of idiocy

--- Conversions.java
+++ Conversions.java
@@ -1,7 +1,7 @@
-package exporting;
+package test;
 
 import java.util.Scanner;
-/*
+/* @version 1.2
  * @author N/A
  * This program converts Fahrenheit to Celsius
  */
@@ -28,14 +28,16 @@ public class Conversions {
 		double temp = 0;
 		String type = "2";
 		Scanner scan = new Scanner(System.in);
+		
 		while(1 == 1 ){
 		boolean t = false;
 		while(t == false){
-		System.out.println("What type of conversion do you want to do? Fahrenheight to Celsius, (f-c)");
+		System.out.println("What type of conversion do you want to do? Fahrenheit to Celsius, (f-c)");
 		System.out.println("or Celsius to Fahrenheight. (c-f)");
 		type = scan.next();
+		
 		switch(type){
-		case "Stop" : System.exit(0);
+		case "Stop" : scan.close(); System.exit(0);
 		break;
 		case "f-c" : t = true; break;
 		case "c-f" : t = true; break;
@@ -43,30 +45,43 @@ public class Conversions {
 		}
 		}
 		s = false;
+		
 		while(s == false){
+	    boolean l = false; 
 		System.out.println("What temperature do you want to convert?");
 		String temp2 = scan.next();
-		for(int i = 0; i <= temp2.length(); i++){
+		switch(temp2.charAt(0)){
+		 case '+': case '-':  l = true; break;
+		 default : break;
+		}
+		
+		for(int i = 0; i < temp2.length(); i++){
 		//Number overflow exception V1
-		if(temp2.length() >= -Integer.MAX_VALUE / 10)	{
+			if(l == true){
+				l = false; 
+				i++;
+			}
+		if(temp2.length() <= -Integer.MAX_VALUE / 10 || temp2.length() >= Integer.MAX_VALUE / 10 )	{
 			s = false;
 			break;
 		}
-		boolean l = true;
+		boolean m = true;
 		//Stop using letters V1
+		System.out.println(i);
+		System.out.println(temp2.charAt(i));
 		switch(temp2.charAt(i)){
-		case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': l = false; s = true; break;
-		default : l = false; break;
+		case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': case '.': s = true; break;
+        default : m = false; s = false; break;
 		}		
-		if(l == false)break;
+		if(m == false){ s = false; System.out.println("Break");break;}
 		}
 		if(s == true){
-		temp = Integer.parseInt(temp2);
+			//Number overflow exception V2
+			if(temp2.length() <= -Integer.MAX_VALUE / 10 || temp2.length() >= Integer.MAX_VALUE / 10 )	{
+				System.out.println("Try again, this number is too large");
+			}
+		temp = Double.parseDouble(temp2);
 		Conversions.math(temp, type);
-		}
-		//Number overflow exception V2
-		else if(temp2.length() >= -Integer.MAX_VALUE / 10)	{
-			System.out.print("Try again, this number is too large");
 		}
 		//Stop using letters V2
 		else{
